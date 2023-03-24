b8902224cbb45018757ec02f8d7890c8c0ab8db0
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-03-24 14:39:02 +0900

    Create Save Activity
    create SaveActivity.java
    create activity_save.xml
    
    have to create view model

---
+++ app/src/main/java/com/example/bbiyongi/SaveActivity.java
@@ -0,0 +1,30 @@
+package com.example.bbiyongi;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SaveActivity extends AppCompatActivity {
+
+    VideoView video;
+    TextView btn_to_police;
+    ImageView btn_back_notifications;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save);
+
+        btn_back_notifications = (ImageView) findViewById(R.id.btn_back_notifications);
+        btn_back_notifications.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
