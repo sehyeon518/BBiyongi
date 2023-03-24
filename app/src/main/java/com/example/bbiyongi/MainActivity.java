7b2a806f5632a789d941793ab937f4dbbd74bd78
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-03-24 12:32:36 +0900

    Update main activity
    View 객체 생성
    Set View click event

--- app/src/main/java/com/example/bbiyongi/MainActivity.java
+++ app/src/main/java/com/example/bbiyongi/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.bbiyongi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.bbiyongi.notifications.NotificationsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView btn_noticifations;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_noticifations = (TextView) findViewById(R.id.btn_notifications);
+        btn_noticifations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
