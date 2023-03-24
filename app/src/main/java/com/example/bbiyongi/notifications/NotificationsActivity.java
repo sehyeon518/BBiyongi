cebb854383de6e1d37aed291cf037f3f3c1bfe2b
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-03-24 15:26:58 +0900

    Add Save Activity in manifest file
    Set Notification btn click event
    save btn -> go to save Activity

--- app/src/main/java/com/example/bbiyongi/notifications/NotificationsActivity.java
+++ app/src/main/java/com/example/bbiyongi/notifications/NotificationsActivity.java
@@ -1,15 +1,20 @@
 package com.example.bbiyongi.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bbiyongi.MainActivity;
 import com.example.bbiyongi.R;
+import com.example.bbiyongi.SaveActivity;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,6 +22,7 @@ import java.util.Date;
 public class NotificationsActivity extends AppCompatActivity {
 
     ImageView btn_back_main;
+    TextView btn_go_save;
     ArrayList<Notification> notificationArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +43,21 @@ public class NotificationsActivity extends AppCompatActivity {
 
         list_view.setAdapter(myAdapter);
 
-//        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-//            @Override
-//            public void onItemClick(AdapterView parent, View v, int position, long id){
-//                Toast.makeText(getApplicationContext(),
-//                        myAdapter.getItem(position).getMovieName(),
-//                        Toast.LENGTH_LONG).show();
-//            }
-//        });
+        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                Toast.makeText(getApplicationContext(),
+                        myAdapter.getItem(position).getDate().toString(), Toast.LENGTH_LONG).show();
+                btn_go_save = (TextView) findViewById(R.id.btn_go_save);
+                btn_go_save.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), SaveActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
     }
 
     public void InitializeData()
