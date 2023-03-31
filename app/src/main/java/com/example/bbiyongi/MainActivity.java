b25914be3b03fdf77ca9782f36706e490ec9bf8a
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-03-31 11:15:30 +0900

    Delete Unnecessary in MainActivity
    Delete unnecessary type casting
    code -> lambda

--- app/src/main/java/com/example/bbiyongi/MainActivity.java
+++ app/src/main/java/com/example/bbiyongi/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bbiyongi.notifications.NotificationsActivity;
@@ -17,13 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_notifications = (TextView) findViewById(R.id.btn_notifications);
-        btn_notifications.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
-                startActivity(intent);
-            }
+        btn_notifications = findViewById(R.id.btn_notifications);
+        btn_notifications.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
+            startActivity(intent);
+        });
+
         });
     }
 }
