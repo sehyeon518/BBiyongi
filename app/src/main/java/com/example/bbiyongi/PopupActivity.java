5c11742a3e9f97cc0d30cb7f3744466129bfcb1c
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-04-01 09:48:17 +0900

    팝업액티비티 내 버튼 이벤트 추가
    날짜 불러오기, 위치 불러오기, 확인 버튼 클릭 이벤트 추가

--- app/src/main/java/com/example/bbiyongi/PopupActivity.java
+++ app/src/main/java/com/example/bbiyongi/PopupActivity.java
@@ -2,14 +2,18 @@ package com.example.bbiyongi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PopupActivity extends Activity {
 
     EditText edit_msg;
     TextView btn_set_return;
+    TextView btn_get_pos;
+    TextView btn_get_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,25 @@ public class PopupActivity extends Activity {
 
         edit_msg = findViewById(R.id.edit_msg);
         btn_set_return = findViewById(R.id.btn_set_return);
-        btn_set_return.setOnClickListener(v -> finish());
+        btn_set_return.setOnClickListener(v -> {
+            // 간편 신고 메시지 저장 기능 추가
+            Toast.makeText(getApplicationContext(), "저장되었습니다", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+
+        btn_get_pos = findViewById(R.id.btn_get_pos);
+        btn_get_pos.setOnClickListener(v -> {
+            String msg = edit_msg.getText().toString();
+            msg = msg + "OO시 OO구 OO로 OO";
+            edit_msg.setText(msg);
+        });
+
+        btn_get_date = findViewById(R.id.btn_get_date);
+        btn_get_date.setOnClickListener(v -> {
+            String msg = edit_msg.getText().toString();
+            msg = msg + "OOOO년 OO월 OO일 OO시 OO분 OO초";
+            edit_msg.setText(msg);
+        });
         //데이터 가져오기
 //        String data = intent.getStringExtra("data");
 //        edit_msg.setText(data);
