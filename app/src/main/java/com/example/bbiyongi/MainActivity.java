f22c6f97433184aa1c33675d165d5f1590763375
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-04-02 10:03:52 +0900

    Check permission in MainActivity
    MainActivity 문자메시지 권한 확인 함수 checkPermission() 추가

--- app/src/main/java/com/example/bbiyongi/MainActivity.java
+++ app/src/main/java/com/example/bbiyongi/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.bbiyongi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bbiyongi.notifications.NotificationsActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String[] permissionList = new String[] {Manifest.permission.SEND_SMS};
 
     TextView btn_notifications;
     ImageView btn_setting;
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
 
         btn_notifications = findViewById(R.id.btn_notifications);
         btn_notifications.setOnClickListener(v -> {
@@ -31,5 +37,19 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), PopupActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String permission : permissionList) {
+                if (checkCallingOrSelfPermission(permission) == PackageManager.PERMISSION_DENIED)
+                    requestPermissions(permissionList, 0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
